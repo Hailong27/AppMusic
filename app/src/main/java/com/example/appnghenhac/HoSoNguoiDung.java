ef83e8efd67821585355818959ddac12c9ab1279
Hailong27 <89600044+Hailong27@users.noreply.github.com>
2023-04-16 17:48:48 +0700

    frame 6

---
+++ app/src/main/java/com/example/appnghenhac/HoSoNguoiDung.java
@@ -0,0 +1,14 @@
+package com.example.appnghenhac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HoSoNguoiDung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ho_so_nguoi_dung);
+    }
+}
